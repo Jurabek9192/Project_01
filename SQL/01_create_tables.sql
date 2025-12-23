85175f411009eb0e1e6bd38559c30f8b4e328606
MR_SEVINCHOFF <jurabekseinchov19940422@gmail.com>
2025-12-23 23:21:44 +0500

    Update 01_create_tables.sql

--- SQL/01_create_tables.sql
+++ SQL/01_create_tables.sql
@@ -1,3 +1,7 @@
+
+-- Here I created table to load dataset from csv file
+-- Здесь я создал таблицу, чтобы загрузить данные из csv-фацла
+
 CREATE TABLE retail_sales (
     transaction_id     VARCHAR(50),
     order_date         DATE,
@@ -10,7 +14,12 @@ CREATE TABLE retail_sales (
     total_amount       NUMERIC(12,2)
 );
 
+-- After importing data I check
+-- После загрузки проверил
+
 SELECT * FROM retail_sales;
 
-SELECT COUNT(*) FROM retail_sales;
+-- Checking how many rows are imported
+-- Проверка количества полученных строк
 
+SELECT COUNT(*) FROM retail_sales;
